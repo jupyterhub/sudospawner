865c3f909bc891ebf437b006b64ee0852d6a0cc4
Min RK <benjaminrk@gmail.com>
2018-06-19 15:49:46 +0200

    update setup.py with long_description_content_type

--- setup.py
+++ setup.py
@@ -4,69 +4,77 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-#-----------------------------------------------------------------------------
-# Minimal Python version sanity check (from IPython/Jupyterhub)
-#-----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython/JupyterHub)
 from __future__ import print_function
 
 import os
 import sys
 
 v = sys.version_info
-if v[:2] < (3,3):
+if v[:2] < (3, 3):
     error = "ERROR: Jupyter Hub requires Python version 3.3 or above."
     print(error, file=sys.stderr)
     sys.exit(1)
 
 
-if os.name in ('nt', 'dos'):
+if os.name in ("nt", "dos"):
     error = "ERROR: Windows is not supported"
     print(error, file=sys.stderr)
 
 # At least we're on the python version we need, move on.
+from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg
 
-from distutils.core import setup
-if any(cmd in sys.argv for cmd in ['bdist_wheel', 'develop']):
-    import setuptools
+
+class bdist_egg_disabled(bdist_egg):
+    """Disabled version of bdist_egg
+
+    Prevents setup.py install from performing setuptools' default easy_install,
+    which it should never ever do.
+    """
+
+    def run(self):
+        sys.exit(
+            "Aborting implicit building of eggs. Use `pip install .`"
+            " to install from source."
+        )
+
 
 pjoin = os.path.join
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the current package version.
 version_ns = {}
-with open(pjoin(here, 'sudospawner/version.py')) as f:
+with open(pjoin(here, "sudospawner/version.py")) as f:
     exec(f.read(), {}, version_ns)
 
+with open("README.md") as f:
+    long_description = f.read()
 
 setup_args = dict(
-    name                = 'sudospawner',
-    packages            = ['sudospawner'],
-    scripts             = ['scripts/sudospawner'],
-    version             = version_ns['__version__'],
-    description         = """SudoSpawner: A custom spawner for JupyterHub.""",
-    long_description    = "Spawn single-user servers with sudo.",
-    author              = "Jupyter Development Team",
-    author_email        = "jupyter@googlegroups.com",
-    url                 = "http://jupyter.org",
-    license             = "BSD",
-    platforms           = "Linux, Mac OS X",
-    keywords            = ['Interactive', 'Interpreter', 'Shell', 'Web'],
-    classifiers         = [
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+    name="sudospawner",
+    packages=["sudospawner"],
+    scripts=["scripts/sudospawner"],
+    version=version_ns["__version__"],
+    description="SudoSpawner: Spawner for JupyterHub using sudo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jupyter Development Team",
+    author_email="jupyter@googlegroups.com",
+    url="https://jupyter.org",
+    license="BSD",
+    platforms="Linux, Mac OS X",
+    keywords=["Interactive", "Interpreter", "Shell", "Web"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
     ],
+    install_requires=["jupyterhub>=0.7", "notebook"],
 )
 
-# setuptools requirements
-if 'setuptools' in sys.modules:
-    setup_args['install_requires'] = install_requires = [
-        'jupyterhub>=0.7',
-        'notebook',
-    ]
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
