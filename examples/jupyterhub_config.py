7952b02429b87abad32bf1162113dafe6f6a8e73
Cody Scot <cody.j.b.scott@gmail.com>
2018-10-26 10:32:31 -0400

    Replace debug_mediator with mediator_log_level

--- examples/jupyterhub_config.py
+++ examples/jupyterhub_config.py
@@ -5,5 +5,5 @@ c = get_config()
 # use the sudo spawner
 c.JupyterHub.spawner_class = 'sudospawner.SudoSpawner'
 
-c.SudoSpawner.debug_mediator = True
+c.SudoSpawner.mediator_log_level = "DEBUG"
 c.JupyterHub.log_level = 10
